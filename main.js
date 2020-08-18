7c5f43a8c77467c9e33496b859fc2006d40690fa
Daniel Chamorro <dach2013@hotmail.com>
2020-08-17 19:13:11 -0500

    Inclusión contenido items

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('#sidebarCollapse').on('click', function () {
+    $('#sidebar').toggleClass('active');
+    $(this).toggleClass('active');
+  });
+});
+
+function showItems(titulo) {
+  $("#navBarTitle").html(titulo);
+}
+
+
+
